b2210cfa00bf72317ae1a328d9154a2864069137
julia.machado <julia.machado@globalsys.com.br>
2023-04-14 10:50:21 -0300

    Projeto feito para download do arquivo CSV, referente aos dados do Painel COVID do estado do ES.
    A automação faz o download do arquivo e verifica se o mesmo já foi finalizado ou não.

---
+++ webScrapping.py
@@ -0,0 +1,24 @@
+
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from VerificaDownload import *
+
+"""
+    A janela de download do Chrome que será aberta pela classe VerificaDownload
+    DEVE ficar aberta, não podemos mudar de aba enquanto o download está sendo feito, uma vez que 
+    a verificação é feita nessa página
+"""
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+elemento = VerificaDownload(driver)
+
+driver.get('https://coronavirus.es.gov.br/painel-covid-19-es')
+
+ele = driver.find_element(By.XPATH, '//*[@id="layout-wrapper"]/div[3]/div/article/div/div/div/div/div/div['
+
+                                           '1]/p/a[1]')
+elemento.arquivo_nome(ele, tempo_espera=3000)
